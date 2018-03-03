2d4ae6cf334a9b6082e1f102a9b9e8aa78968f5b
fangyh09 <fangyh09@gmail.com>
2018-03-03 11:50:32 +0800

    add shell

---
+++ testnet.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+declare -a arr=("www.baidu.com" "www.github.com" 
+"www.126.com")
+
+echo "Testing..."
+for i in "${arr[@]}"
+do
+    time=`ping -c 2 "$i" | tail -1| awk '{print $4}' | cut -d '/' -f 3`
+    echo "$i it's time = $time s"
+done
+
+echo "Finish..."
+
