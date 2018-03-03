979d419e265a5b347e3ffeb14fa95dfb7910aab7
Coder_Hong <9389269+Fangyh09@users.noreply.github.com>
2018-03-03 13:49:25 +0800

    Update net-status.sh

--- net-status.sh
+++ net-status.sh
@@ -8,7 +8,7 @@ echo "Testing..."
 for i in "${arr[@]}"
 do
     time=`ping -c 2 "$i" | tail -1| awk '{print $4}' | cut -d '/' -f 3`
-    echo "$i it's time = $time s"
+    echo "$i it's time = $time ms"
 done
 
 echo "Finish..."
